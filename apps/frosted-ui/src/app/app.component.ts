a308e4c16dcd349f6f72b0953b859cca1a89cac3
Theodore Obonye <ltobonye@gmail.com>
2022-08-12 19:33:31 +0200

    scaffold nx workspace

---
+++ apps/frosted-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'frosted-ui-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'frosted-ui';
+}
