e8883429cdcd328398e179e27dd6a981239b3c6e
Theodore Obonye <ltobonye@gmail.com>
2022-08-24 20:06:17 +0200

    chore: simplify storybook

--- apps/frosted-ui-e2e/src/e2e/app.component.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('frosted-ui', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=appcomponent--primary'));
-  it('should render the component', () => {
-    cy.get('frosted-ui-root').should('exist');
-  });
-});
--- apps/frosted-ui-e2e/src/e2e/nx-welcome.component.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('frosted-ui', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=nxwelcomecomponent--primary'));
-  it('should render the component', () => {
-    cy.get('frosted-ui-nx-welcome').should('exist');
-  });
-});
